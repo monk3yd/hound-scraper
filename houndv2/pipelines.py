8d309de1270618ca3d5a9a1b4c46df0c8342faaf
monk3yd <jegajardog@gmail.com>
2022-04-27 20:44:12 -0400

    Clean pipeline

--- houndv2/pipelines.py
+++ houndv2/pipelines.py
@@ -5,7 +5,6 @@
 
 import logging
 import sqlite3
-import csv
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
@@ -13,7 +12,6 @@ from itemadapter import ItemAdapter
 
 class SQLlitePipeline(object):
     collection_name = "casetracker"
-    case_uid = 1
 
     def open_spider(self, spider):
         # Connect to db
@@ -21,52 +19,31 @@ class SQLlitePipeline(object):
         self.db = self.connection.cursor()
 
         # Create table 
-        try:
-            self.db.execute('''
-                CREATE TABLE casetracker(
-                    row_id INTEGER AUTOINCREMENT,
-                    case_uid INTEGER,
-                    folio INTEGER,
-                    tramite TEXT,
-                    descripcion TEXT,
-                    fecha TEXT,
-                    sala TEXT,
-                    estado TEXT,
-                    link TEXT
-                );
-            ''')
-        except sqlite3.OperationalError:
-            pass
+        # try:
+        self.db.execute('''
+            CREATE TABLE cases_movs (
+                row_id AUTOINCREMENT PRIMARY KEY,
+                case_uid INTEGER,
+                folio INTEGER,
+                tramite TEXT,
+                descripcion TEXT,
+                fecha TEXT,
+                sala TEXT,
+                estado TEXT,
+                link TEXT
+            );
+        ''')
+    # except sqlite3.OperationalError:
+        #     pass
 
     def close_spider(self, spider):
-        # logging.info("Spider closed from pipeline")
         # Close db
         self.connection.close()
-        # print(self.cases)
-        
     
     def process_item(self, item, spider):
-        # self.tracking_data = item["tracking_data"]
-        # # 'tracking_data': {'COMPETENCIA': 'Corte Apelaciones', 'CORTE': 'San Miguel', 'TIPO': 'Protección', 'ROL': 7780, 'AÑO': 2020, 'ARCHIVADA': True}
-        # case = [
-        #     item["folio"],
-        #     # item["anexo"],
-        #     item["tramite"],
-        #     item["descripcion"],
-        #     item["fecha"],
-        #     item["sala"],
-        #     item["estado"],
-        #     item["doc"],
-        #     # item["georef"],
-        # ]
-        
-        # with open(f"causas/C.A. de {self.tracking_data['CORTE']}-{self.tracking_data['ROL']}-{self.tracking_data['AÑO']}.csv", "a") as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow(case)
-        # item["tracking_data"]
-        # # Insert into db
+        # Insert into db
         self.db.execute('''
-            INSERT INTO casetracker (case_uid, folio, tramite, descripcion, fecha, sala, estado, link) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT INTO cases_movs (case_uid, folio, tramite, descripcion, fecha, sala, estado, link) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
         ''', (
             int(item["case_uid"]), 
             int(item["folio"]),
