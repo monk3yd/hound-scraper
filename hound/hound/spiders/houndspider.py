2c60654310ec4e736301bbd35c226cb984b51780
monk3yd <jegajardog@gmail.com>
2022-09-15 16:35:32 -0300

    Instantiate playwrights' page to wait for selector, use coroutines (async needed)

--- hound/hound/spiders/houndspider.py
+++ hound/hound/spiders/houndspider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy_playwright.page import PageCoroutine  # async
 
 
 class HoundspiderSpider(scrapy.Spider):
@@ -6,9 +7,15 @@ class HoundspiderSpider(scrapy.Spider):
 
     def start_requests(self):
         url = "https://oficinajudicialvirtual.pjud.cl/indexN.php"
-        yield scrapy.Request(url, meta={"playwright": True})
+        yield scrapy.Request(url, meta={
+            "playwright": True,  # use playwright when performing request
+            "playwright_include_page": True,  # Instantiate playwrights' page object
+            "playwright_page_coroutines": [
+                PageCoroutine("wait_for_selector", "button#btnConConsulta")  # wait for selector to load before returning response
+            ]
+        })
 
-    def parse(self, response):
+    async def parse(self, response):
         yield {
             "text": response.text
         }
